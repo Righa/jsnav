882c0da934cb3ef5c54de28df7c101e3bc452291
Righa <54580993+Righa@users.noreply.github.com>
2020-04-27 18:04:41 +0300

    adding
    animated nav

---
+++ home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<nav>
+		<div class="logo">
+			<h4>NOTHING HERE</h4>
+		</div>
+		<ul class="nav-links">
+			<li><a href="#">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Projects</a></li>
+			<li><a href="#">Login</a></li>
+		</ul>
+		<div class="burger">
+			<div class="line1"></div>
+			<div class="line2"></div>
+			<div class="line3"></div>
+		</div>
+	</nav>
+
+	<script src="app.js"></script>
+</body>
+</html>
